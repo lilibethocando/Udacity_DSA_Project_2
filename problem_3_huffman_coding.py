031128609d2d11596f6990e0d35aa87964c37202
Lilibeth Ocando <lilibethocando@gmail.com>
2022-08-07 14:56:06 -0500

    Add more edge cases to Huffman Coding problem

--- problem_3_huffman_coding.py
+++ problem_3_huffman_coding.py
@@ -84,20 +84,32 @@ def huffman_decoding(data, tree):
     return decoded_string
 
 
+
+
+def test_cases(test_case):
+    if test_case != "":
+        #a_great_sentence = "The bird is the word"
+
+        print("The size of the data is: {}\n".format(sys.getsizeof(test_case)))
+        print("The content of the data is: {}\n".format(test_case))
+
+        encoded_data, tree = huffman_encoding(test_case)
+
+        print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+        print("The content of the encoded data is: {}\n".format(encoded_data))
+
+        decoded_data = huffman_decoding(encoded_data, tree)
+
+        print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+        print("The content of the encoded data is: {}\n".format(decoded_data))
+
+    else:
+        print("Your string is empty")
+
+
 if __name__ == "__main__":
     codes = {}
 
-    a_great_sentence = "The bird is the word"
+    test_cases("The bird is the word")
 
-    print("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
-    print("The content of the data is: {}\n".format(a_great_sentence))
-
-    encoded_data, tree = huffman_encoding(a_great_sentence)
-
-    print("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
-    print("The content of the encoded data is: {}\n".format(encoded_data))
-
-    decoded_data = huffman_decoding(encoded_data, tree)
-
-    print("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
-    print("The content of the encoded data is: {}\n".format(decoded_data))
+    test_cases("")
